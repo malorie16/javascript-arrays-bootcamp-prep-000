f43e2e377d9119893bda53f3e78c39d76832bfce
malorie16 <malorie18@gmail.com>
2018-04-20 14:36:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  return array.unshift('element')
 }
 
 function addElementToEndOfArray(array, element) {
@@ -13,7 +13,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  return array.push('element')
 }
 
 function accessElementInArray(array, index) {
@@ -21,5 +21,5 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  
+
 }
